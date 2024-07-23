4d93557fb2c29eb0dfba42ef69c70dac0bed4a79
ianmark8 <ianmarkmorga07@gmail.com>
2024-07-23 18:45:14 +0800

    initial commit

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+    console.log('Server listening on port 3000!');
 }
 );
