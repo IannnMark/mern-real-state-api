ab7959e6a510954c8d9a973b01979810caebd7f4
ianmark8 <ianmarkmorga07@gmail.com>
2024-07-23 19:17:51 +0800

    connect to database

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(process.env.DATABASE).then(() => {
+    console.log('Connected to MongoDB');
+})
+    .catch((err) => {
+        console.log(err);
+    });
 
 const app = express();
 
