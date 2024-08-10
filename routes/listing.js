83e7cb9c6e7748208ce6b32d107df90dcf4b171d
ianmark8 <ianmarkmorga07@gmail.com>
2024-08-10 11:33:00 +0800

    delete user listing

--- routes/listing.js
+++ routes/listing.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const verifyToken = require('../utils/verifyUser');
 const router = express.Router();
-const { createListing } = require('../controllers/listingController');
+const { createListing, deleteListing } = require('../controllers/listingController');
 
 router.post('/create', verifyToken, createListing);
+router.delete('/delete/:id', verifyToken, deleteListing);
 
 
 module.exports = router;
