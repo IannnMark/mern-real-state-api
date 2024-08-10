51becf5b3abf5e26d6c73e98962421724c851026
ianmark8 <ianmarkmorga07@gmail.com>
2024-08-10 15:17:38 +0800

    create update listing route

--- routes/listing.js
+++ routes/listing.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const verifyToken = require('../utils/verifyUser');
 const router = express.Router();
-const { createListing, deleteListing } = require('../controllers/listingController');
+const { createListing, deleteListing, updateListing } = require('../controllers/listingController');
 
 router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.put('/update/:id', verifyToken, updateListing);
 
 
 module.exports = router;
