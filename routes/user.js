033f7c1ad245f5dc2f9d34b103ba43375834a918
ianmark8 <ianmarkmorga07@gmail.com>
2024-08-12 21:14:06 +0800

    add  contact landlord functionality

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { test, updateUser, deleteUser, getUserListings } = require('../controllers/userController.js');
+const { test, updateUser, deleteUser, getUserListings, getUser } = require('../controllers/userController.js');
 const verifyToken = require('../utils/verifyUser.js');
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get('/test', test);
 router.post('/update/:id', verifyToken, updateUser);
 router.delete('/delete/:id', verifyToken, deleteUser);
-router.get("/listings/:id", verifyToken, getUserListings)
+router.get('/listings/:id', verifyToken, getUserListings)
+router.get('/:id', verifyToken, getUser)
 
 module.exports = router;
