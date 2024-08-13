7430d16df397b7abb6c4d79352b6cae1f05a0d45
ianmark8 <ianmarkmorga07@gmail.com>
2024-08-13 10:14:39 +0800

    create search api

--- controllers/listingController.js
+++ controllers/listingController.js
@@ -65,4 +65,59 @@ exports.getListing = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-}   
+}
+
+
+exports.getListings = async (req, res, next) => {
+    try {
+        const limit = parseInt(req.query.limit) || 9;
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        let offer = req.query.offer;
+
+        //pag hindi sinelect yung offer makikita yung parin lahat ng listing. kapag sinelect naman yung offer makikita lang yung mismong merong offer
+        if (offer === undefined || offer === 'false') {
+            offer = { $in: [false, true] };
+        }
+
+        let furnished = req.query.furnished;
+
+        //same logic sa offer
+        if (furnished === undefined || furnished === 'false') {
+            furnished = { $in: [false, true] };
+        }
+
+        let parking = req.query.parking;
+
+        if (parking === undefined || parking === 'false') {
+            parking = { $in: [false, true] }
+        }
+
+        let type = req.query.type;
+
+        if (type === undefined || type === 'all') {
+            type = { $in: ['sale', 'rent'] };
+        }
+
+        const searchTerm = req.body.searchTerm || '';
+
+        const sort = req.body.sort || 'createdAt';
+
+        const order = req.body.order || "desc";
+
+        const listings = await Listing.find({
+            // yung i dito ay pwede kahit upper or lower case yung letter
+            name: { $regex: searchTerm, $options: 'i' },
+            offer,
+            furnished,
+            parking,
+            type,
+        }).sort(
+            { [sort]: order }
+        ).limit(limit).skip(startIndex);
+
+        return res.status(200).json(listings);
+
+    } catch (error) {
+        next(error);
+    }
+}
--- routes/listing.js
+++ routes/listing.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const verifyToken = require('../utils/verifyUser');
 const router = express.Router();
-const { createListing, deleteListing, updateListing, getListing } = require('../controllers/listingController');
+const { createListing, deleteListing, updateListing, getListing, getListings } = require('../controllers/listingController');
 
 router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.put('/update/:id', verifyToken, updateListing);
 router.get('/get/:id', getListing);
+router.get("/get", getListings);
 
 
 module.exports = router;
